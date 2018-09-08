ad0ca4fd88989a8b86544239d60ce960be192e14
Carl Albrecht <carl@areait.com.au>
2018-09-08 14:17:15 +1000

    Added simple card for initiating the image capture process

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { Camera } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
